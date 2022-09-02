d023f153eb0a766198981ec3cca71c4c6675da29
Felipe Souza <fgsouza93@gmail.com>
2022-09-01 23:10:32 -0300

    customize tab bar

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import Coracao from "assets/coracao.svg";
+import Home from "assets/home.svg";
+
 import ProducerRoutes from "./ProducerRoutes";
 import TopProducersRoutes from "./TopProducersRoutes";
 
@@ -10,7 +13,23 @@ const Tab = createBottomTabNavigator();
 const AppRoutes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            let Icon = Home;
+            if (route.name === "Top Producers Tab") {
+              Icon = Coracao;
+            }
+
+            return <Icon color={color} />;
+          },
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarActiveTintColor: "#2A9F85",
+          tabBarInactiveTintColor: "#C7C7C7",
+        })}>
         <Tab.Screen name={"Home Tab"} component={ProducerRoutes} />
         <Tab.Screen name={"Top Producers Tab"} component={TopProducersRoutes} />
       </Tab.Navigator>
